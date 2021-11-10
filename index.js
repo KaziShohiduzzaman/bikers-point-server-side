39cf90942b82a08196b66414394296d423c4d2f2
KaziShohiduzzaman <kazishohiduzzaman786@gmail.com>
2021-11-11 04:42:07 +0600

    Admin done

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
+const e = require('express');
 
 
 const app = express();
@@ -22,6 +23,7 @@ async function run() {
         const database = client.db('bikeWala');
         const productsCollection = database.collection('products');
         const orderCollection = database.collection('orders');
+        const usersCollection = database.collection('users')
 
         //Post api for Products 
         app.post('/products', async (req, res) => {
@@ -81,6 +83,43 @@ async function run() {
             const query = { _id: ObjectId(id) }
             const result = await orderCollection.findOne(query);
             res.send(result);
+        })
+
+        //post for store email and name
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            res.json(result)
+        })
+
+        //PUT for upsert email and name
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const options = { upsert: true };
+            const updateDoc = { $set: user };
+            const result = await usersCollection.updateOne(filter, updateDoc, options);
+            res.json(result);
+        });
+
+        //put for admin
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = { $set: { role: 'admin' } };
+            const result = await usersCollection.updateOne(filter, updateDoc);
+            res.json(result)
+        })
+
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            let isAdmin = false;
+            if (user?.role === 'admin') {
+                isAdmin = true;
+            }
+            res.json({ admin: isAdmin });
         })
 
 
